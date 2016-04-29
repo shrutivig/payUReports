bd9265e24a30046cfb6ee9f72a61c72448dbf104
Shruti Vig <shruti.vig@ShrutiVigMac.local>
2016-04-29 12:31:14 +0530

    Initialising Application

---
+++ app/src/androidTest/java/reports/payu/com/app/payureports/ApplicationTest.java
@@ -0,0 +1,13 @@
+package reports.payu.com.app.payureports;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
